d7a08319f6fc72d4d7f8ae941f2e25a85841ef4d
benmelz <ben@melz.me>
2023-02-12 14:26:32 -0500

    chore: set up flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  add_flash_types :primary, :secondary, :success, :danger, :warning, :info, :light, :dark
+
   protect_from_forgery
 
   def root; end
