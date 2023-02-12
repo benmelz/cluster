deb9485b703298fe2c974a0fea4b219abe7f086e
benmelz <ben@melz.me>
2023-02-12 16:25:20 -0500

    feat: user profiles (#8)
    * chore: disable registration
    
    * spec: User::Profile
    
    * feat: User::Profile
    
    * feat: user name in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :recoverable, :registerable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  has_one :profile, dependent: :destroy, required: true, validate: true
+  accepts_nested_attributes_for :profile, update_only: true
+
+  delegate :first_name, :last_name, to: :profile
 end
--- factories/users.rb
+++ factories/users.rb
@@ -2,8 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    profile { build :user_profile, user: instance }
     sequence(:email) { |n| "user#{n}@test.com" }
     password { 'password' }
-    password_confirmation { 'password' }
+    password_confirmation { password }
   end
 end
