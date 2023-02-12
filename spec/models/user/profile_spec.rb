1c223acfad5ada65e36b5835e663d8ab54d7e6bf
benmelz <ben@melz.me>
2023-02-12 16:20:56 -0500

    spec: User::Profile

---
+++ spec/models/user/profile_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User::Profile do
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    subject { build(:user_profile) }
+
+    it { is_expected.to validate_uniqueness_of(:user_id) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+  end
+end
